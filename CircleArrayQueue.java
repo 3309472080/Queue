a64086d033d174068abcb2019b9fb0dd087eae12
3309472080 <75651333+3309472080@users.noreply.github.com>
2021-01-10 13:09:03 +0800

    Add files via upload
    单队列
    环形队列

---
+++ CircleArrayQueue.java
@@ -0,0 +1,127 @@
+package duilie;
+
+import java.util.Scanner;
+
+public class CircleArrayQueue {
+    public static void main(String[] args) {
+        //实例化队列类
+        ArrayQueue1 arrayQueue = new ArrayQueue1(3);
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        char key = ' ';
+        while (loop){
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):添加数据到队列");
+            System.out.println("g(get):从队列取出数据");
+            System.out.println("h(head):查看队列头的数据");
+            System.out.println("请输入");
+            key = scanner.next().charAt(0);
+            switch (key){
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("请输出一个数字");
+                    int value = scanner.nextInt();
+                    try{
+                        arrayQueue.addQueue(value);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+
+                    break;
+                case 'g':
+                    try {
+                        int res = arrayQueue.getQueue();
+                        System.out.printf("取出的数据是%d\n",res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    System.out.println(arrayQueue.headQueue());
+                    break;
+                case 'e':
+                    loop = false;
+            }
+        }
+
+    }
+}
+//使用数组模拟队列-编写一个ArrayQueue类
+class  ArrayQueue1 {
+    private int maxSize;//表示数组的最大容量
+    /*  front变量的含义做一个调整，front就指向队列的第一个元素，也就是说arr[front]就是队列的第一个元素
+        front 的初始值=0
+    */
+    private int front;//队列头
+    /*
+    rear变量的含义做一个调整：rear指向队列最后一个元素的后一个位置，因为希望空出一个空间作为约定
+    rear的初始值=0
+    */
+    private int rear;//队列尾
+    private int arr[];//该数组用于存放数据，模拟队列
+
+    //创建队列的构造器
+    public ArrayQueue1 (int arrMaxSize){
+        maxSize = arrMaxSize;
+        arr = new int[maxSize];
+    }
+
+    //判断队列是否满
+    public boolean isFull(){
+        return (rear+1) % maxSize == front;
+//        return rear % maxSize == 0;
+    }
+    //判断队列是否为空
+    public boolean isEmpty(){
+        return rear == front;
+    }
+    //添加数据到队列
+    public void addQueue(int n){
+        //判断队列是否满
+        if(isFull()){
+            System.out.println("队列满，不加入数据");
+            return;
+        }
+
+        arr[rear] = n;
+        rear = (rear+1) % maxSize;
+
+    }
+    //获取队列的数据，出队列
+    public int getQueue(){
+        if(isEmpty()){
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        //1.先把front 的值保留到一个临时变量
+        // 2. 将front后移
+        //3.将临时的变量返回
+        int value = arr[front];
+        front = (front+1)%maxSize;
+        return value;
+    }
+    public void showQueue(){
+        //遍历
+        if(isEmpty()){
+            System.out.println("队列空，无数据");
+            return;
+        }
+        //思路：从front开始遍历，遍历多少元素
+
+        for (int i = front; i < front+size(); i++) {
+            System.out.printf("arr[%d]=%d\n",i%maxSize,arr[i%maxSize]);//第一个i = [%d],第二个arr[i] = %d
+        }
+    }
+    public int size(){
+        return  (rear+maxSize-front)%maxSize;
+    }
+    //显示队列的头数据，不是取出数据
+    public int headQueue(){
+        if(isEmpty()){
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        return arr[front];
+    }
+}
